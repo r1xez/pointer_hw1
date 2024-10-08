990244efa9789806f8ce9c364f9c8657f68e6cc7
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 22:43:43 +0300

    Ex4 from hw was done

--- pointer_hw1/main.cpp
+++ pointer_hw1/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 
+    int a, b;
+    char op;
+    std::cout << "Enter an operation (+, -, *, /): ";
+    std::cin >> op;
+    std::cout << "Enter value of a: ";
+    std::cin >> a;
+    std::cout << "Enter value of b: ";
+    std::cin >> b;
 
-	return 0;
+    int* pA = &a;
+    int* pB = &b;
+    int result;
+
+    switch (op) {
+    case '+':
+        result = *pA + *pB;
+        break;
+    case '-':
+        result = *pA - *pB;
+        break;
+    case '*':
+        result = *pA * *pB;
+        break;
+    case '/':
+        result = *pA / *pB;
+        break;
+    default:
+        std::cout << "Invalid operation" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
 }
