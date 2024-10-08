c63c584593f20945d4d6aa21a332d2307a8950e2
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 22:47:40 +0300

    Ex5 from hw was done

--- pointer_hw1/main.cpp
+++ pointer_hw1/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
+
+    const int SIZE = 6;
+    int arr[SIZE] = { 1, 2, 3, 4, 5,10};
+    int sum = 0;
+
+    int* ptr = arr;
+
+    for (int i = 0; i < SIZE; i++) {
+        sum += *ptr;
+        ptr++;
+    }
+
+    cout << "Sum of the array elements: " << sum << endl;
 
 
-	return 0;
+
+    return 0;
 }
