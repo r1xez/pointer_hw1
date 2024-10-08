5e70d447b3d6b9bae7187a7f4dd6ad40863beccd
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 22:39:54 +0300

    Ex1 form hw was done

--- pointer_hw1/main.cpp
+++ pointer_hw1/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+
+    int a, b;
+
+    cout << "Enter value of A:";
+    cin >> a;
+    cout << "Enter value of B:";
+    cin >> b;
 
 
-	return 0;
+    int* pA = &a;
+    int* pB = &b;
+
+    if (*pA > *pB)
+        cout << "A is bigger than B" << endl;
+    else if (*pA < *pB)
+        cout << "A is smaller than B" << endl;
+    else
+        cout << "A is equal to B" << endl;
+
+
+    return 0;
 }
