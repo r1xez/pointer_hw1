9990f4ba7d0c8ba2fa00003a398ddc00f35642cb
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 22:41:32 +0300

    Ex2 from hw was done

--- pointer_hw1/main.cpp
+++ pointer_hw1/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+
+    int a, b;
+
+    cout << "Enter value of A:";
+    cin >> a;
 
 
-	return 0;
+    int* num = &a;
+
+    if (*num > 0)
+    {
+        cout << "Value of A is positive" << endl;
+    }
+    else if (*num < 0)
+    {
+        cout << "Value of A is negative" << endl;
+    }
+    else
+    {
+        cout << "Value of A is zero" << endl;
+    }
+
+
+    return 0;
 }
