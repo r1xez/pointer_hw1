7715ee5456d95ccd68acc392096d30bff79f7f96
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 22:42:50 +0300

    Ex3 from hw was done

--- pointer_hw1/main.cpp
+++ pointer_hw1/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+
+	int a, b;
+
+	cout << "Enter value of a: ";
+	cin >> a;
+	cout << "Enter value of b: ";
+	cin >> b;
+
+	int* p1 = &a;
+	int* p2 = &b;
+
+	int* temp = p1;
+	p1 = p2;
+	p2 = temp;
+
+	cout << "a: " << *p1 << endl;
 
 
 	return 0;
